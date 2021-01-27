a00b444352ee8c4eea7de8e8842d3b0feca94cdc
KIM WONHEE <soyoung0007@naver.com>
2021-01-27 21:52:34 +0900

    i am rich

--- udemy_practice/lib/main.dart
+++ udemy_practice/lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(new MyApp());
+  runApp(
+    MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar:AppBar(
+          leading: new Icon(Icons.payment,color:Colors.blue[300],size:30.0),
+          titleSpacing: 0,
+          title:
+          Text(
+            'I am rich',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+            textAlign:TextAlign.center,
+          ),
+          backgroundColor: Colors.white,
+        ),
+      body:Center(
+        image:AssetImage('images/diamond.jpg'),
+      )
+      ),
+
+    ),
+  );
 
 }
 
-class  MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+
